4a64e48540cb9fd88e7a1efbc6a75e8050dd6702
abdelghanysd <aabdelghany@singledigits.com>
2024-05-01 12:00:27 +0300

    add section 9

---
+++ section9/sec9.cpp
@@ -0,0 +1,32 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//vector<vector<string> > vec;
+//
+//int rawan_hash(string str){
+//    int res =1;
+//    for(int i =0; i<3; i++){
+//        res *=str[i];
+//    }
+//    return  res;
+//}
+//
+//int main(){
+//
+//    vec.resize(10000);
+//
+//    vec[rawan_hash("ahmed")].push_back("ahmed");
+//
+//    vec['m' - 'a'].push_back("mona");
+//
+////    vector<int> vec;
+////    vec.push_back(15);
+////    vec.push_back(7);
+////    vec.push_back(19);
+////    vec.push_back(57);
+////    vec.push_back(63);
+////    vec.push_back(3);
+////
+////    sort(vec.begin(), vec.end());
+//}
